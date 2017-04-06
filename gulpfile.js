f66d3edc8f2da0e1b0626a22e9ad5984b2daba46
Ivan Medina <medinadiazivan@gmail.com>
2017-04-06 12:58:02 -0500

    js task on gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 var gulp = require("gulp");
+var browserify = require("gulp-browserify");
+var rename = require("gulp-rename");
 var sass = require("gulp-sass");
 
 var config = {
 	source: "./src",
-	build: "./dist",
-	buildCss: "main.css"
+	build: "./dist"
 };
 
 gulp.task("sass", function() {
@@ -13,4 +14,11 @@ gulp.task("sass", function() {
         	outputStyle: "compressed"
         }).on("error", sass.logError))
         .pipe(gulp.dest(config.build + "/css"));
+});
+
+gulp.task("js", function() {
+	gulp.src(config.source + "/js/app.js")
+		.pipe(browserify())
+		.pipe(rename('bundle.js'))
+		.pipe(gulp.dest(config.build + "/js"));
 });
