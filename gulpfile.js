d234ff8917ee2c4648a1d5d51812c42e1739cb94
Ivan Medina <medinadiazivan@gmail.com>
2017-04-06 13:00:13 -0500

    Move HTML files from src to dist

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,26 @@ var rename = require("gulp-rename");
 var sass = require("gulp-sass");
 
 var config = {
-	source: "./src",
-	build: "./dist"
+	source: "./src/",
+	build: "./dist/"
 };
 
 gulp.task("sass", function() {
-    gulp.src(config.source + "/scss/**/*.scss")
+    gulp.src(config.source + "scss/**/*.scss")
         .pipe(sass({
         	outputStyle: "compressed"
         }).on("error", sass.logError))
-        .pipe(gulp.dest(config.build + "/css"));
+        .pipe(gulp.dest(config.build + "css"));
 });
 
 gulp.task("js", function() {
-	gulp.src(config.source + "/js/app.js")
+	gulp.src(config.source + "js/app.js")
 		.pipe(browserify())
-		.pipe(rename('bundle.js'))
-		.pipe(gulp.dest(config.build + "/js"));
+		.pipe(rename("bundle.js"))
+		.pipe(gulp.dest(config.build + "js"));
+});
+
+gulp.task("html", function() {
+	gulp.src(config.source + "**/*.html")
+		.pipe(gulp.dest(config.build));
 });
